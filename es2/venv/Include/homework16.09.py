b762e4e455f2e381d390f970f3f52ac1cd56c9c5
erba994 <ltosi@edu.hse.ru>
2018-09-13 20:37:58 +0300

    домашнее задание 16.09

--- es2/venv/Include/homework16.09.py
+++ es2/venv/Include/homework16.09.py
@@ -1,7 +1,7 @@
 a1 = input("напишите слово: ")
 a2 = enumerate(a1)
 l1 = list()
-for count,letter in a2:
+for count, letter in a2:
     if count % 2 == 1 and letter != "а" and letter != "к":
         l1.append(letter)
 print(l1)
@@ -10,9 +10,15 @@ l2 = list()
 b1 = int(input("напишите число: "))
 for number in range(b1):
     b2 = input("напишите слово: ")
-    if b2 != ("программирование"):
+    if b2 != "программирование":
         l2.append(b2)
     else:
         break
 if len(l2) == b1:
     print(l2)
+
+a3 = input("напишите слово: ")
+b3 = (list(a3)[:int(len(a3)/2)])
+c3 = (list(a3)[int(len(a3)/2):])
+d3 = c3[::-1]
+print("".join(b3) + "".join(d3))
