7f2208c433bb721cb11884360fb51f81cd7ff509
erba994 <ltosi@edu.hse.ru>
2018-09-24 20:51:27 +0300

    починил код в 30.09 домашнее задание

--- es2/venv/Include/homework30.09 - caesarcipher.py
+++ es2/venv/Include/homework30.09 - caesarcipher.py
@@ -2,6 +2,7 @@ alphabet = ("а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и",
             "й", "к", "л", "м", "н", "о", "п", "р", "с", "т",
             "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь",
             "э", "ю", "я")
+#with slice operator on a tuple, is possible to implement a cipher index input field easily
 cipher = alphabet[1:] + alphabet[:1]
 
 cleancaesar = input("напишите предложение: ").split(" ")
@@ -10,6 +11,8 @@ caesar = " ".join(cleancaesar)
 for letter in caesar:
     if letter in alphabet:
         print(cipher[alphabet.index(letter)], end="")
+    elif letter.lower() in alphabet:
+        print(cipher[alphabet.index(letter.lower())].upper(), end="")
     else:
         print(letter, end="")
 
